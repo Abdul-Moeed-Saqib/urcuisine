fefd989162b8b44c08b39ff42dcd2230f2f8d75f
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-08-29 21:18:43 -0400

    Created backend structure and implemented models

---
+++ backend/models/postModel.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Post struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	UserID      primitive.ObjectID `bson:"user_id,omitempty"`
+	Title       string             `bson:"title,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	VideoURL    string             `bson:"video_url,omitempty"`
+	Recipe      string             `bson:"recipe,omitempty"`
+	Country     string             `bson:"country,omitempty"`
+	Likes       int                `bson:"likes,omitempty"`
+	Dislikes    int                `bson:"dislikes,omitempty"`
+	Comments    []Comment          `bson:"comments,omitempty"`
+}
+
+type Comment struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	UserID  primitive.ObjectID `bson:"user_id,omitempty"`
+	Text    string             `bson:"text,omitempty"`
+	Created int64              `bson:"created,omitempty"`
+}
