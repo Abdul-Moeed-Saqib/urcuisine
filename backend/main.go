13a93aba6c14ee006bf8a2f166b17933a0bb0c07
Abdul Moeed Saqib <59030881+Abdul-Moeed-Saqib@users.noreply.github.com>
2024-08-29 22:44:53 -0400

    updated the server

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Abdul-Moeed-Saqib/urcuisine-backend/config"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Couldn't load the env file")
+	}
+
+	config.ConnectDB() // connecting to the database
+
+	router := mux.NewRouter()
+
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
